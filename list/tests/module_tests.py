ef21a4156596caca23f1a0f711968c1b654b55a4
Clayton Condie <clayton.condie@gmail.com>
2016-10-19 12:36:03 -0600

    refine unit tests

--- list/tests/module_tests.py
+++ list/tests/module_tests.py
@@ -11,8 +11,8 @@ class UnrolledLinkedList_Test(unittest.TestCase):
 
     def setUp(self):
         self.l = UnrolledLinkedList()
-        # create a unrolled linked list with one node with 5 elements 1-5
-        for i in range(1, 6):
+        # create a unrolled linked list with one node with 5 elements 0-4
+        for i in range(0, 5):
             self.l.append(i)
 
     def tearDown(self):
@@ -27,28 +27,58 @@ class UnrolledLinkedList_Test(unittest.TestCase):
         """
         self.assertEqual(self.l.max_node_capacity, 16)
 
-    def test_get_item(self):
-        self.l.max_node_capacity = 5
-        self.assertEqual(self.l[1], 2)
-        self.assertEqual(self.l[-2], 4)
+    def test_get_item_single_node(self):
+        # check that index 1 is 1 [0, |1|, 2, 3, 4]
+        self.assertEqual(self.l[1], 1)
+
+    def test_get_item_single_node_neg_index(self):
+        # check that the second to last el is 3 [0, 1, 2, |3| ,4]
+        self.assertEqual(self.l[-2], 3)
+
+    def test_get_item_exception(self):
+        # attempt to get an item outside of max index
         with self.assertRaises(IndexError) as cm:
-            dummy = self.l[10]
+            dummy = self.l[self.l.length + 1]
+
+    def test_get_item_two_node(self):
+        self.l.max_node_capacity = 5
+        for i in range(5, 7):
+            self.l.append(i)
+        self.assertEqual(self.l[5], 5)
+
+    def test_get_item_two_node_neg_index(self):
+        self.l.max_node_capacity = 5
+        for i in range(5, 7):
+            self.l.append(i)
+        self.assertEqual(self.l[-1], 6)
 
     def test_del_item(self):
-        self.assertEqual(self.l[1], 2)
+        # check that index 1 is 1
+        self.assertEqual(self.l[1], 1)
+        # delete el 1 at index 1
         del self.l[1]
-        self.assertEqual(self.l[1], 3)
-        self.assertEqual(self.l[-2], 4)
-        del self.l[-2]
-        self.assertEqual(self.l[-2], 3)
+        # check that index 1 is now 2
+        self.assertEqual(self.l.length, 4)
+        self.assertEqual(self.l[1], 2)
+
+    def test_del_item_exception(self):
         with self.assertRaises(IndexError) as cm:
-            del self.l[5]
+            del self.l[7]
+
+    def test_del_item_neg_index(self):
+        # check that the second to last index i 4 [0, 1, 2, 3, 4]
+        self.assertEqual(self.l[-2], 3)
+        # delete the second to last element [0, 1, 2, |3|, 4]
+        del self.l[-2]
+        # check that the second to last index is now 2 [0, 1, |2|, 4]
+        self.assertEqual(self.l.length, 4)
+        self.assertEqual(self.l[-2], 2)
 
     def test_append(self):
         # shrink the max capacity to something more manageable
         self.l.max_node_capacity = 5
         self.assertEqual(self.l.length, 5)
-        # add element 6
-        self.l.append(6)
+        # add element 5
+        self.l.append(5)
         self.assertEqual(self.l.length, 6)
-        self.assertEqual(str(self.l), '{[1, 2],[3, 4, 5, 6]}')
+        self.assertEqual(str(self.l), '{[0, 1],[2, 3, 4, 5]}')
