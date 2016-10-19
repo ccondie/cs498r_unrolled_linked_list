a2fb3ff341993e44d306592aa0a6151db07e0e19
Clayton Condie <clayton.condie@gmail.com>
2016-10-18 20:04:07 -0600

    checkin

--- list/unrolled_linked_list/module.py
+++ list/unrolled_linked_list/module.py
@@ -1,9 +1,32 @@
-
-
 class UnrolledLinkedList():
     """This is the class name you should use. You should also have a
     max_node_capacity. Also, you should remove this comment and possibly
     replace it with your own
     """
+
     def __init__(self, max_node_capacity=16):
         self.max_node_capacity = max_node_capacity
+
+    def __delitem__(self, index):
+        pass
+
+    def __getitem__(self, index):
+        pass
+
+    def __setitem__(self, key, value):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __reversed__(self):
+        pass
+
+    def __contains__(self, obj):
+        pass
